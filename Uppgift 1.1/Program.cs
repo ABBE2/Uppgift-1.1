27980422bc2ce9ca038ed787316a20d83761beeb
BaoLanNguyen <bang03001@utb.vaxjo.se>
2023-01-18 09:59:54 +0100

    Add project files.

---
+++ Uppgift 1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Namn till programmet.
+namespace uppgift_1._1
+{
+    class program1
+    {
+
+        static void Main(string[] args)
+        {
+            // 'Console' innebär skärmen och 'WriteLine' innebär att den ska skriva ut en text eller ett värde och sen byta rad.
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+
+            // För att skriva " och \ behöver du först använda \ innan du skriver det andra tecknet. 
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\ ");
+
+        }
+           
+
+    }
+
+}
